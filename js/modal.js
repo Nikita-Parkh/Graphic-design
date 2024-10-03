d3414c44dc3a18caecc5208882cf4f8bae88629b
Nikita-Parkh <nikitos4754@gmail.com>
2024-10-03 23:10:32 +0300

    ././.

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+const closeButton = document.querySelector('.header-container-second__button');
+const modal = document.querySelector('.header-container-second');
+
+closeButton.addEventListener('click', function() {
+    modal.classList.add('hidden');
+});
